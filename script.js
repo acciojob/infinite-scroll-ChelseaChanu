9b934d96b9bf41f88cc3195adc77e2633db0cfa3
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-20 23:18:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let i = 1;
+for(;i<=10;i++){
+	const item = document.createElement("li");
+	item.innerText = `Item ${i}`;
+	list.appendChild(item);
+}
 
+list.addEventListener("scroll",()=>{
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		for(;i<=2;i++){
+			const item = document.createElement("li");
+			item.innerText = `Item ${i}`;
+			list.appendChild(item);
+		}
+	}
+});
